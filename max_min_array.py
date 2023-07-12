d3cd6d9db1421830104ab61d41f64167265f5bf7
Bharathi Kannan <112495342+bharathi0811@users.noreply.github.com>
2023-07-12 16:28:42 +0530

    max_min_array.py

--- max_min_array.py
+++ max_min_array.py
@@ -1,12 +1,14 @@
 max_=0
 array=list(map(int,input("Enter the array:").split()))
-for i in array:
-    if max_<i:
-        max_=i
-min_ =array[0]       
-for j in array:
-    if min_<j:
-        continue
-    else:
-        min_=j
-print(max_+min_)
+def max_min(array):
+    for i in array:
+        if max_<i:
+            max_=i
+    min_ =array[0]       
+    for j in array:
+        if min_<j:
+            continue
+        else:
+            min_=j
+    return max_+min_
+print(max_min(array))
